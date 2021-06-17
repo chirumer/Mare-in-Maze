b0a8ac80ca008cab3717b3ff31ac3d2028cebb26
chirumer <chirumer@users.noreply.github.com>
2021-06-17 19:20:03 +0530

    now use SDL_image to load images

--- source.c
+++ source.c
@@ -1,13 +1,15 @@
 // ---- headers ----
 
 #include <SDL2/SDL.h>
-// SDL2 
+ // SDL2 
+#include <SDL2/SDL_image.h>
+ // for loading images other than bitmap
 #include <stdio.h>
-// printf(), scanf()
+ // printf(), scanf()
 #include <stdlib.h>
-// exit(), EXIT_FAILURE
+ // exit(), EXIT_FAILURE
 #include <stdbool.h>
-// bool
+ // bool
 
 // ---- constants ----
 
@@ -15,62 +17,70 @@
 const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 500;
 
-const char* assets_path = "./assets";
-    // path to the assets folder
+const char * assets_path = "./assets";
+// path to the assets folder
 
 // ---- function declarations ----
 
-void initialize(SDL_Window** window, SDL_Renderer** window_renderer);
-    // create window and surface
+void initialize(SDL_Window ** window, SDL_Renderer ** window_renderer);
+// create window and surface
 
-void cleanup(SDL_Window** window, SDL_Renderer** window_renderer);
-    // destroy window and exit SDL
-    
-bool show_instructions(SDL_Window* window, SDL_Renderer* window_renderer);
-    // display an instructions page
-    // returns whether user has closed program
-    
+void cleanup(SDL_Window ** window, SDL_Renderer ** window_renderer);
+// destroy window and exit SDL
+
+bool show_instructions(SDL_Window * window, SDL_Renderer * window_renderer);
+// display an instructions page
+// returns whether user has closed program
+
 // ---- main ----
 
-int main(int argc, char* args[]) {
+int main(int argc, char * args[]) {
 
     // initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 
-	fprintf(
-	    stderr,
-	    "SDL_Init error: %s\n",
-	    SDL_GetError()
-	);
-	exit(EXIT_FAILURE);
+        fprintf(
+            stderr,
+            "SDL_Init error: %s\n",
+            SDL_GetError()
+        );
+        exit(EXIT_FAILURE);
     }
-	
+
     // initialize necessary things
-    SDL_Window* window = NULL; 
-    SDL_Renderer* window_renderer = NULL;
-    initialize(&window, &window_renderer);
+    SDL_Window * window = NULL;
+    SDL_Renderer * window_renderer = NULL;
+    initialize( & window, & window_renderer);
 
     // introduce user to game
     if (!show_instructions(window, window_renderer)) {
-	cleanup(&window, &window_renderer);
-	return EXIT_SUCCESS;
+        cleanup( & window, & window_renderer);
+        return EXIT_SUCCESS;
     }
 
     // release resources
-    cleanup(&window, &window_renderer);
+    cleanup( & window, & window_renderer);
 
     return EXIT_SUCCESS;
 }
 
 // ---- function definitions ----
 
-bool show_instructions(SDL_Window* window, SDL_Renderer* window_renderer) {
+bool show_instructions(SDL_Window * window, SDL_Renderer * window_renderer) {
 
     bool is_exit = false;
 
     //  load and display instructions image
-    SDL_Surface* image = SDL_LoadBMP("./assets/instructions.bmp");
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(window_renderer, image);
+    SDL_Surface * image = IMG_Load("./assets/instructions.png");
+    if (image == NULL) {
+        printf(
+            stderr,
+            "IMG_Load error: %s\n",
+            IMG_GetError()
+        );
+        exit(EXIT_FAILURE);
+    }
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(window_renderer, image);
     SDL_FreeSurface(image);
     SDL_RenderCopy(window_renderer, texture, NULL, NULL);
     SDL_RenderPresent(window_renderer);
@@ -79,75 +89,89 @@ bool show_instructions(SDL_Window* window, SDL_Renderer* window_renderer) {
     bool is_done = false;
     while (!is_done) {
 
-	SDL_Event event;
-        SDL_WaitEvent(&event);
-	switch(event.type) {
-	    case SDL_QUIT:
-		fprintf(stderr, "quitting..\n");
-		is_done = true;
-		is_exit = true;
-		break;
-	    case SDL_KEYUP:
-		is_done = true;
-		break;
-	}
-	if (is_done) { break; }
-	while (!SDL_PollEvent(&event)) {
-	    switch(event.type) {
-		case SDL_QUIT:
-		    fprintf(stderr, "quitting..\n");
-		    is_done = true;
-		    is_exit = true;
-		    break;
-		case SDL_KEYUP:
-		    is_done = true;
-		    break;
-	    }
-	    if (is_done) { break; }
-	}
+        SDL_Event event;
+        SDL_WaitEvent( & event);
+        switch (event.type) {
+        case SDL_QUIT:
+            fprintf(stderr, "quitting..\n");
+            is_done = true;
+            is_exit = true;
+            break;
+        case SDL_KEYUP:
+            is_done = true;
+            break;
+        }
+        if (is_done) {
+            break;
+        }
+        while (!SDL_PollEvent( & event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                fprintf(stderr, "quitting..\n");
+                is_done = true;
+                is_exit = true;
+                break;
+            case SDL_KEYUP:
+                is_done = true;
+                break;
+            }
+            if (is_done) {
+                break;
+            }
+        }
     }
     SDL_DestroyTexture(texture);
     return is_exit;
 }
 
-void initialize(SDL_Window** window, SDL_Renderer** window_renderer) {
+void initialize(SDL_Window ** window, SDL_Renderer ** window_renderer) {
 
     // create our window
-    *window = SDL_CreateWindow(
-	"Mare in Maze",
-	SDL_WINDOWPOS_UNDEFINED,
-	SDL_WINDOWPOS_UNDEFINED,
-	SCREEN_WIDTH,
-	SCREEN_HEIGHT,
-	SDL_WINDOW_SHOWN
+    * window = SDL_CreateWindow(
+        "Mare in Maze",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN
     );
-    if (*window == NULL) { // something went wrong
+    if ( * window == NULL) { // something went wrong
 
-	fprintf(
-	    stderr,
-	    "SDL_CreateWindow error: %s\n",
-	    SDL_GetError()
-	);
-	exit(EXIT_FAILURE);
+        fprintf(
+            stderr,
+            "SDL_CreateWindow error: %s\n",
+            SDL_GetError()
+        );
+        exit(EXIT_FAILURE);
     }
 
-    *window_renderer = SDL_CreateRenderer(*window, -1, 0);
-    if (*window_renderer == NULL) { // something went wrong
+    * window_renderer = SDL_CreateRenderer( * window, -1, 0);
+    if ( * window_renderer == NULL) { // something went wrong
 
-	fprintf(
-	    stderr,
-	    "SDL_GetWindowSurface error: %s\n",
-	    SDL_GetError()
-	);
-	exit(EXIT_FAILURE);
+        fprintf(
+            stderr,
+            "SDL_GetWindowSurface error: %s\n",
+            SDL_GetError()
+        );
+        exit(EXIT_FAILURE);
+    }
+
+    int img_flags = IMG_INIT_PNG;
+    if (!(IMG_Init(img_flags) & img_flags)) {
+        fprintf(
+            stderr,
+            "IMG_Init error: %s\n",
+            IMG_GetError()
+        );
+        exit(EXIT_FAILURE);
     }
 }
 
-void cleanup(SDL_Window** window, SDL_Renderer** window_renderer) {
+void cleanup(SDL_Window ** window, SDL_Renderer ** window_renderer) {
 
-    SDL_DestroyWindow(*window);
-    *window = NULL;
-    SDL_DestroyRenderer(*window_renderer);
-    *window_renderer = NULL;
+    SDL_DestroyWindow( * window);
+    * window = NULL;
+    SDL_DestroyRenderer( * window_renderer);
+    * window_renderer = NULL;
     SDL_Quit();
 }
